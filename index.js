6d6650a6c7e7811383caa17a4dfc3cfd53ea6b78
bouldercoder9 <alma9011@colorado.edu>
2020-02-23 21:32:43 -0700

    init commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+require('dotenv').config();
+const app = express();
+
+app.use(express.static('client'));
+
+MongoClient.connect(process.env.MONGO_CONNECT, { useUnifiedTopology: true }, (err, database) => {
+    if (err) return console.log("CONNECTION" + err);
+    //Connect to our cluster
+    db = database.db('Cluster0');
+
+    app.listen(8080, () => {
+        console.log("Server running on localhost:8080");
+    });
+
+});
+
+app.get('/a/test', (req,res) => {
+    let test = "This is a test";
+    res.json(test);
+    console.log('Test string');
+});
+
+app.get('/', (req,res) => {
+    res.send('Hello World!')
+});
